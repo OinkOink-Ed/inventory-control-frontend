5364e54abbb1df38d3019185dee60e0e4811116e
OinkOink-Ed <oinkoinkpw@gmail.com>
2024-12-08 20:55:35 +0300

    Добавил tailwind, shadcn, zustand

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,13 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./providers/queryClientProvider";
+import { ThemeProvider } from "./providers/themeProvider";
 
 export function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div>App</div>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <div>App</div>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { getApiSwagger } from "./getApiSwagger";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +15,9 @@ export default defineConfig({
     },
     react(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
