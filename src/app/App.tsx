948df92e34db44b72bdfcaff90a7a8e72d4ba554
OinkOink-Ed <oinkoinkpw@gmail.com>
2024-12-08 21:28:37 +0300

    stage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./providers/queryClientProvider";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "./providers/ThemeProvider";
+import { ThemeProvider } from "./ThemeProvider";
+import { queryClientInstans } from "./queryClientInstans";
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClientInstans}>
         <div>
           App
           <Toaster />
