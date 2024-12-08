ff2bbd6267a30f91649b7a26295cd90a7bd16ece
OinkOink-Ed <oinkoinkpw@gmail.com>
2024-12-08 21:14:20 +0300

    Добавил toast от shadcn, исправил жалобы eslint на Fast Refresh React в файлах от shadcn

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./providers/queryClientProvider";
-import { ThemeProvider } from "./providers/themeProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "./providers/ThemeProvider";
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <QueryClientProvider client={queryClient}>
-        <div>App</div>
+        <div>
+          App
+          <Toaster />
+        </div>
       </QueryClientProvider>
     </ThemeProvider>
   );
