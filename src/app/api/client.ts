975805bacd48ba518d2e33145f0a4a284255afae
OinkOink-Ed <oinkoinkpw@gmail.com>
2025-01-16 17:41:41 +0300

    stage

---
+++ src/app/api/client.ts
@@ -0,0 +1,54 @@
+// Написано с божъей помощью и примером от kubb.dev
+
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+
+export interface RequestConfig<TData = unknown> {
+  url?: string;
+  method: "GET" | "PUT" | "PATCH" | "POST" | "DELETE";
+  params?: object;
+  data?: TData | FormData;
+  responseType?:
+    | "arraybuffer"
+    | "blob"
+    | "document"
+    | "json"
+    | "text"
+    | "stream";
+  signal?: AbortSignal;
+  headers?: AxiosRequestConfig["headers"];
+}
+
+export interface ResponseConfig<TData = unknown> {
+  data: TData;
+  status: number;
+  statusText: string;
+  headers?: AxiosResponse["headers"];
+}
+
+export const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = "gdsgsdg"; /* Получать токен из сторадж*/
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export const axiosClient = async <
+  TData,
+  TError = unknown,
+  TVariables = unknown,
+>(
+  config: RequestConfig<TVariables>,
+): Promise<ResponseConfig<TData>> => {
+  const response = await axiosInstance
+    .request({ ...config })
+    .catch((e: AxiosError<TError>) => {
+      throw e;
+    });
+
+  return response;
+};
+
+export default axiosClient;
