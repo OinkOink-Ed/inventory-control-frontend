98e0904673069f10183d5d041212746bee88c6f9
oinkink-ed <oinkoinkpw@gmail.com>
2025-01-18 13:40:34 +0300

    stage

--- src/app/routes/loaders/AdminRoute.tsx
+++ src/app/routes/loaders/AdminRoute.tsx
@@ -2,7 +2,7 @@ import { CustomErrorForbidden } from "@/app/Errors/CustomErrorForbidden";
 import { decryptedProfile } from "@/app/helpers/decryptedProfile";
 
 export default function AdminRoute() {
-  if (decryptedProfile()?.role.roleName === "Admin") {
+  if (decryptedProfile()?.role.roleName !== "Admin") {
     //Проблема в том, что каждый переход запоминает в истории навигации, нужно что-то с этим сделать
     //Либо редирект придумать по таймауту в errorBondary (в целом редирект точно нужен мне кажется - прочёл ошибку и тебя вернуло)
     const error = new CustomErrorForbidden("Нет доступа к странице!", 403);
